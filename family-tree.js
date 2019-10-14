7627bcaac487cde45f6315c628d64dd66005503d
Paul Perez <colopaul@hotmail.com>
2019-10-14 18:44:12 -0400

    done

--- family-tree.js
+++ family-tree.js
@@ -1,5 +1,37 @@
 class FamilyTree {
-  constructor () {}
+  constructor (name) {
+    if (typeof name !== 'string') {
+      throw new Error('need a real name');
+    }
+    this.name = name;
+    this.children = []
+  }
+  insert(name){
+    const newMember = new FamilyTree(name);
+    this.children.push(newMember);
+    // console.log(newMember)
+    return newMember;
+  }
+  familySize(){
+    // console.log(this.children)
+    return 1 + this.children.length;
+  }
+
+  findMember(){
+    
+    if (this.name === name) {
+
+      return this;
+    }
+    
+    return this.children.find( child => {
+      console.log(child)
+      return child.findMember(name);
+    });
+}
+  log(){
+    // console.log(this.name)
+  }
 }
 
 module.exports = FamilyTree;
